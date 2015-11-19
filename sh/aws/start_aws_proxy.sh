dfb14129bfa2daf2451118383675ce2352c99955
Tony Deng <wolf.deng@gmail.com>
2015-11-19 10:19:40 +0800

    更改证书目录

--- sh/aws/start_aws_proxy.sh
+++ sh/aws/start_aws_proxy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i ~/.aws_key/duoqu_aws_key.pem -p 8015 -CfNg -L 8089:127.0.0.1:8888 ec2-user@52.68.169.148
+ssh -i ~/.ssh/aws_key/duoqu_aws_key.pem -p 8015 -CfNg -L 8089:127.0.0.1:8888 ec2-user@52.68.169.148
